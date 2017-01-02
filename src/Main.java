4819f5c92bc75c5e299fd6e5e9b87d41902c1207
MinHyeJun <m2j9702@naver.com>
2017-01-02 21:10:27 +0900

    5단 출력 코드

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, Java!");
+        for(int i = 1; i < 10; i++)
+            System.out.printf("5 * %d = %d\n", i, 5*i);
     }
 }
