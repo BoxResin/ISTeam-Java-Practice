1e52fe3c97adaa2450069af254af82d026e018c7
MinHyeJun <m2j9702@naver.com>
2017-01-03 15:08:16 +0900

    문자열 쪼개기 수정

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,21 @@ public class Main
     public static void main(String[] args)
     {
         int s_s = 0, s_f;
+        char c;
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
 
-        for(int i = 0; i < str.length(); i++)
+        for (int i = 0; i < str.length(); i++)
         {
-            if (str.charAt(i) == ' ')
+            c = str.charAt(i);
+            if (c == ' ')
             {
                 s_f = i;
-                System.out.println(str.substring(s_s,s_f));
-                s_s = s_f+1;
-                i++;
+                if(str.charAt(i-1)!=' ')
+                System.out.println(str.substring(s_s, s_f));
+                s_s = s_f + 1;
             }
         }
-        System.out.println(str.substring(s_s,str.length()));
+        System.out.println(str.substring(s_s, str.length()));
     }
 }
