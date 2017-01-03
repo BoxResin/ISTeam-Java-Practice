3916e14c1c1862f9fe4e914ed9e352c5f3124d4f
BoxResin <boxresin@gmail.com>
2017-01-03 14:54:18 +0900

    문자열 쪼개기 모범 답안

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, Java!");
-    }
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+
+		String[] words = input.split(" ");
+		for (int i = 0; i < words.length; i++)
+		{
+			System.out.println(words[i]);
+		}
+	}
 }
