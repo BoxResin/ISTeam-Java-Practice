3ac074970980529c1cc88f7b41717a7a3f798876
loverhdiddl <loverhdiddl@gmail.com>
2017-01-03 15:53:53 +0900

    두 점 사이의 거리 구하기

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,17 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        for(int i=0; i<str.length(); ++i){
-            if(str.charAt(i)!=' ')
-            {
-                System.out.printf("%c", str.charAt(i));
-            }
-            else{
-                System.out.print('\n');
-            }
-        }
+        int x1, y1, x2, y2;
+
+        System.out.print("x1, y1 좌표 입력: ");
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
+
+        System.out.print("x2, y2 좌표 입력: ");
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
+
+        System.out.printf("거리: %f", Math.sqrt(((x1-x2)*(x1-x2))- ((y1-y2)*(y1-y2))));
 
     }
 }
