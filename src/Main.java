60c0a2d62889c586ddcc3eafca1a9b5843fa38b2
MinHyeJun <m2j9702@naver.com>
2017-01-03 15:20:30 +0900

    별 찍기

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int s_s = 0, s_f;
-        char c;
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        int num = scanner.nextInt();
 
-        for (int i = 0; i < str.length(); i++)
+        for(int i = 0; i < num; i++)
         {
-            c = str.charAt(i);
-            if (c == ' ')
-            {
-                s_f = i;
-                if(str.charAt(i-1)!=' ')
-                System.out.println(str.substring(s_s, s_f));
-                s_s = s_f + 1;
-            }
+            for (int j = 0; j <= i; j++)
+                System.out.print("*");
+
+            System.out.print("\n");
         }
-        System.out.println(str.substring(s_s, str.length()));
+
     }
 }
