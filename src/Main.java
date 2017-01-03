d40537326dbab9e6c6208bf2be9a295d22bc9169
loverhdiddl <loverhdiddl@gmail.com>
2017-01-03 16:51:22 +0900

    랜덤 값 맞추기 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 /**
  * Created by HyeRyeong on 2016-12-30.
  */
@@ -7,18 +9,24 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int rand = ((int) ((Math.random() * 100) + 1));
         Scanner scanner = new Scanner(System.in);
-        int x1, y1, x2, y2;
+        int iNum;
 
-        System.out.print("x1, y1 좌표 입력: ");
-        x1 = scanner.nextInt();
-        y1 = scanner.nextInt();
 
-        System.out.print("x2, y2 좌표 입력: ");
-        x2 = scanner.nextInt();
-        y2 = scanner.nextInt();
-
-        System.out.printf("거리: %f", Math.sqrt(((x1-x2)*(x1-x2))+ ((y1-y2)*(y1-y2))));
+        while(true){
+            iNum = scanner.nextInt();
+            if(iNum == rand){
+                System.out.println("정답입니다!");
+                exit(1);
+            }
+            else if(iNum > rand){
+                System.out.println("높습니다.");
+            }
+            else{
+                System.out.println("낮습니다.");
+            }
+        }
 
     }
 }
