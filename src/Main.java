e22f4e6ab360de87955f75255b08d6f78a6fa272
MinHyeJun <m2j9702@naver.com>
2017-01-03 12:16:23 +0900

    설정 변경(주석 맞춤법 검사 해제)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        for(int i = 1; i < 10; i++)
-            System.out.printf("5 * %d = %d\n", i, 5*i);
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.printf("%f\n", 10F);
+
+        int x = 10;
+        if (number < 10 || number > 0)
+        {
+            for (int i = 1; i < x; i++)
+            {
+                int result = number * i;
+                System.out.printf("%d * %d = %d\n", number, i, result);
+            }
+        }
+
     }
 }
