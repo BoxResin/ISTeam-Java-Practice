c8a5cc8ca82aa45d639a8ad8b7486ba70ae85f5c
loverhdiddl <loverhdiddl@gmail.com>
2017-01-03 16:06:35 +0900

    scanner 응용

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        int x1, y1, x2, y2;
+        String str = "3 62";
+        Scanner scanner = new Scanner(str);
+        int iNum1, iNum2;
+        int iSum =0;
 
-        System.out.print("x1, y1 좌표 입력: ");
-        x1 = scanner.nextInt();
-        y1 = scanner.nextInt();
+        iNum1 = scanner.nextInt();
+        iNum2 = scanner.nextInt();
 
-        System.out.print("x2, y2 좌표 입력: ");
-        x2 = scanner.nextInt();
-        y2 = scanner.nextInt();
-
-        System.out.printf("거리: %f", Math.sqrt(((x1-x2)*(x1-x2))- ((y1-y2)*(y1-y2))));
+        for(int i= iNum1; i<=iNum2; ++i){
+            iSum += i;
+        }
+        System.out.print(iSum);
 
     }
 }
