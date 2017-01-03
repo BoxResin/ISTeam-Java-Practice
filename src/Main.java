7c549d91658a9020dfa144c6c88d53c10e5da3e9
MinHyeJun <m2j9702@naver.com>
2017-01-03 15:33:13 +0900

    두 점 사이의 거리 구하기

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int x1, x2, y1, y2;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("줄 수 입력: ");
-        int num = scanner.nextInt();
 
-        for(int i = 0; i < num; i++)
-        {
-            for (int j = 0; j <= i; j++)
-                System.out.print("*");
+        System.out.print("x1 y1 좌표 입력: ");
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
 
-            System.out.print("\n");
-        }
+        System.out.print("x1 y1 좌표 입력: ");
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
 
+        System.out.println("거리: " + Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2)));
     }
 }
