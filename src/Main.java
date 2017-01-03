4879fe962c40d481b744b018fcacf371c98f16c6
loverhdiddl <loverhdiddl@gmail.com>
2017-01-03 16:53:47 +0900

    합계 계산기 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
@@ -9,24 +10,55 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int rand = ((int) ((Math.random() * 100) + 1));
+        ArrayList<Integer> array = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int iNum;
+        int iOption;
+        int iSum = 0;
 
+        while (true)
+        {
+            System.out.println("[메뉴]");
+            System.out.println("1. 숫자 추가");
+            System.out.println("2. 숫자 제거");
+            System.out.println("3. 합계 구하기");
+            System.out.println("4. 끝내기");
+            System.out.print("명령 입력: ");
 
-        while(true){
-            iNum = scanner.nextInt();
-            if(iNum == rand){
-                System.out.println("정답입니다!");
-                exit(1);
+            iOption = scanner.nextInt();
+
+            switch (iOption)
+            {
+                case 1:
+                    System.out.print("숫자 입력: ");
+                    iNum = scanner.nextInt();
+                    array.add(iNum);
+                    break;
+                case 2:
+                    System.out.print("삭제할 숫자 위치 입력: ");
+                    iNum = scanner.nextInt();
+                    if(iNum < array.size())
+                    {
+                        array.remove(iNum);
+                    }
+                    else{
+                        System.out.println("ERROR");
+                    }
+                    break;
+                case 3:
+                    for(int i=0; i<array.size(); ++i){
+                        iSum += array.get(i);
+                    }
+                    System.out.printf("합계: %d\n", iSum);
+                    iSum = 0;
+                    break;
+                case 4:
+                    exit(1);
+
             }
-            else if(iNum > rand){
-                System.out.println("높습니다.");
-            }
-            else{
-                System.out.println("낮습니다.");
-            }
+
         }
-
     }
+
+
 }
