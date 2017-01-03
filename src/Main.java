4b97a99598ba57896b9acf39d00e7f85cb25b7a3
MinHyeJun <m2j9702@naver.com>
2017-01-03 16:23:51 +0900

    합계 계산기

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,55 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
 {
+
     public static void main(String[] args)
     {
-        int answer = ((int) (Math.random() * 100 + 1));
-        int input;
-
+        int menu = 0, num;
+        ArrayList<Integer> arrayInt = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("정답: " + answer);
-        System.out.print("입력: ");
-        input = scanner.nextInt();
 
-        while(answer != input)
+        while (true)
         {
-            if(input > answer)
-                System.out.println("높습니다.");
+            System.out.println("\n[메뉴]");
+            System.out.println("1. 숫자 추가");
+            System.out.println("2. 숫자 제거");
+            System.out.println("3. 합계 구하기");
+            System.out.println("4. 끝내기");
+            System.out.print("명령 입력: ");
+            menu = scanner.nextInt();
+
+            if (menu == 1)
+            {
+                System.out.print("\n숫자 입력: ");
+                num = scanner.nextInt();
+                arrayInt.add(num);
+            } else if (menu == 2)
+            {
+                System.out.print("\n삭제할 숫자 위치 입력: ");
+                num = scanner.nextInt();
+                if (num < arrayInt.size())
+                    arrayInt.remove(num);
+                else
+                    System.out.println("해당 위치에 숫자가 없습니다.");
+            } else if (menu == 3)
+            {
+                int sum = 0;
+                for (int i = 0; i < arrayInt.size(); i++)
+                    sum += arrayInt.get(i);
+
+                System.out.println("\n합계: " + sum);
+            } else if (menu == 4)
+                break;
             else
-                System.out.println("낮습니다.");
+                System.out.println("\n잘못된 메뉴 입력입니다.");
 
-            System.out.print("입력: ");
-            input = scanner.nextInt();
         }
+        System.out.println("\n프로그램을 종료합니다.");
 
-        System.out.println("정답입니다!");
     }
+
 }
