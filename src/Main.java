bccaedd539e886274f29fd1e29297c3b4726891b
loverhdiddl <loverhdiddl@gmail.com>
2017-01-03 15:05:59 +0900

    첫 번째 커밋 //문자열 쪼개기

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by HyeRyeong on 2016-12-30.
+ */
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, Java!");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String [] data = str.split(" ");
+        for(int i=0; i<data.length;i++){
+            System.out.println(data[i]);
+        }
     }
 }
