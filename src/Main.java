d7350439479efd1b0070e4b513d3bb73e335c4c5
BoxResin <boxresin@gmail.com>
2017-01-05 14:09:32 +0900

    소문자 전환 모범답안

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,15 @@ import java.util.Scanner;
 public class Main
 {
 	/**
-	 * 찾아 바꾸기 모범 답안
+	 * 소문자 전환 모범 답안
 	 */
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		System.out.printf("문자열 입력: ");
+		System.out.printf("입력: ");
 		String input = scanner.nextLine();
 
-		System.out.printf("찾기: ");
-		String find = scanner.nextLine();
-
-		System.out.printf("바꾸기: ");
-		String change = scanner.nextLine();
-
 		System.out.printf("결과: ");
-		System.out.println(input.replace(find, change));
+		System.out.println(input.toLowerCase());
 	}
 }
