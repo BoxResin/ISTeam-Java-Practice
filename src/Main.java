59489362cd9e79d2943a0fba513628a9d626c759
BoxResin <boxresin@gmail.com>
2017-01-05 14:08:17 +0900

    찾아 바꾸기 모범 답안

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 public class Main
 {
+	/**
+	 * 찾아 바꾸기 모범 답안
+	 */
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("x1, y1 좌표 입력: ");
-		int x1 = scanner.nextInt();
-		int y1 = scanner.nextInt();
+		System.out.printf("문자열 입력: ");
+		String input = scanner.nextLine();
 
-		System.out.print("x2, y2 좌표 입력: ");
-		int x2 = scanner.nextInt();
-		int y2 = scanner.nextInt();
+		System.out.printf("찾기: ");
+		String find = scanner.nextLine();
 
-		System.out.println("거리: " + Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
+		System.out.printf("바꾸기: ");
+		String change = scanner.nextLine();
+
+		System.out.printf("결과: ");
+		System.out.println(input.replace(find, change));
 	}
 }
