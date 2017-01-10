4019a6c0da2e80487164fa4b9185be0fa2610567
BoxResin <boxresin@gmail.com>
2017-01-10 10:09:14 +0900

    프로젝트 초기화

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
-import java.util.Scanner;
-
 public class Main
 {
-	/**
-	 * 문자열 합치기 모범 답안
-	 */
 	public static void main(String[] args)
 	{
-		Scanner scanner = new Scanner(System.in);
-		String result = "";
-
-		while (true)
-		{
-			String input = scanner.nextLine();
-
-			if (input.equals("-1"))
-				break;
-
-			result += input;
-		}
-
-		System.out.printf("결과: " + result);
 	}
 }
