d73256aac736f81a4a7e0d5347301acd87264d03
HyeRyeongSong <loverhdiddl@gmail.com>
2017-01-23 11:39:54 +0900

    thread에서 똑같은 변수를 접근할 경우 -> 결과를 예측할 수 없다 (함부로 쓸 	수 없다

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,36 @@ import java.util.Scanner;
  */
 public class Main
 {
-    public static void main(String[] args)
+    private static int number = 0;
+
+    public static void main(String[] args) throws InterruptedException
     {
         //스레드 : 동시에 여러 코드를 실행하는 것.
 
         //익명 클래스로 오버라이딩해줘야 함 (customizing 가능)
-        Thread thread = new Thread(){
+        Thread thread = new Thread()
+        {
             @Override
-            public void run(){
+            public void run()
+            {
                 //실행할 코드
-                try
-                {
-                    for (int i = 0; i < 10; ++i)
-                    {
-                        System.out.println(i + "번째");
-                        sleep(1000);
-                    }
-                }
-                catch (InterruptedException e){
 
+                for (int i = 0; i < 10; ++i)
+                {
+                    number += i;
                 }
+
+
             }
         };
         thread.start(); //스레드 실행 -> 반드시 start로 호출해야 함
 
-        Scanner scan= new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("숫자를 입력하세요: ");
-        int number = scan.nextInt();
-        System.out.println("number * number = " + number * number);
+        number += scan.nextInt();
+
+        Thread.sleep(3000);
+
+        System.out.println("number = " + number);
     }
 }
