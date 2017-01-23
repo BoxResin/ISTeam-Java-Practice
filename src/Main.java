95951efd526f664ae825d87b5e2ac2464cbcd1f7
HyeRyeongSong <loverhdiddl@gmail.com>
2017-01-23 11:34:02 +0900

    Thread를 익명 클래스로 오버라이딩해줘야 함 (customizing 가능)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by HyeRyeong on 2016-12-30.
  */
@@ -5,16 +7,31 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Thread문에서 발생하는 오류는 반드시  try, catch문으로 잡아줘야 함
-        try
-        {
-            Thread.sleep(3000); //프로그램이 3초간 멈춤
-        } catch (InterruptedException e)    //반드시 호출해야하는 오류
-        {
-            e.printStackTrace();
-        }
+        //스레드 : 동시에 여러 코드를 실행하는 것.
 
+        //익명 클래스로 오버라이딩해줘야 함 (customizing 가능)
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                //실행할 코드
+                try
+                {
+                    for (int i = 0; i < 10; ++i)
+                    {
+                        System.out.println(i + "번째");
+                        sleep(1000);
+                    }
+                }
+                catch (InterruptedException e){
+
+                }
+            }
+        };
+        thread.start(); //스레드 실행 -> 반드시 start로 호출해야 함
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("숫자를 입력하세요: ");
+        int number = scan.nextInt();
+        System.out.println("number * number = " + number * number);
     }
-
-
 }
