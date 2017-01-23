d806e0ac862ee8fa5a7ad4bd468e2faddd8254d0
HyeRyeongSong <loverhdiddl@gmail.com>
2017-01-23 11:16:30 +0900

    try, catch문을 customizing해줄 수 있음 (위임하는 방식 -> 함수 따로 만듬)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,30 +8,28 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //예외 처리
-        Scanner scanner = new Scanner(System.in);
-
-        //#Case 2: 예외처리 전용 문법을 사용한 예외처리 방법
         try
         {
-            //실행할 코드를 쭉 적는다.
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int result = a / b;
-            System.out.println("결과 " + result);
-
+            input();
         } catch (InputMismatchException e)
         {
-            System.out.println("숫자를 입력해주세요.");
-        } catch (ArithmeticException e)
-        {
-            //각 exception 안에서도 각각의 상황의 따른 필터링이 필요함
-            if (e.getMessage().equals("by zero")) //적절한 비교 메시지
-                System.out.println("숫자를 0으로 나눴습니다.");
             e.printStackTrace();
-        } catch (Exception e)
+        } catch (ArithmeticException e)
         {
             e.printStackTrace();
         }
+
+    }
+
+    public static void input() throws InputMismatchException, ArithmeticException
+    {
+        //예외 처리
+        Scanner scanner = new Scanner(System.in);
+
+        //실행할 코드를 쭉 적는다
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int result = a / b;
+        System.out.println("결과: " + result);
     }
 }
