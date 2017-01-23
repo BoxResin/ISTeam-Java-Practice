d279bb94ca75f05aea3fba9652127f30a9691637
HyeRyeongSong <loverhdiddl@gmail.com>
2017-01-23 11:08:48 +0900

    각 exception 안의 각각의 상황에 따른 필터링 필요

--- src/Main.java
+++ src/Main.java
@@ -12,35 +12,23 @@ public class Main
         //예외 처리
         Scanner scanner = new Scanner(System.in);
 
-/*        // str에 "Hello"가 정확히 입력되어야 한다고 가정
-        String str = scanner.next();
-
-        //#Case 1: if문을 사용한 예외처리 방법
-        //정상 입력일 때 동작
-       if (str.equals("Hello"))
-        {
-
-        }
-        //비정상 입력일 때 동작
-        else
-        {
-            System.out.println("Hello를 입력하세요");
-        }
-*/
         //#Case 2: 예외처리 전용 문법을 사용한 예외처리 방법
         try
         {
             //실행할 코드를 쭉 적는다.
-            int number = scanner.nextInt();
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int result = a / b;
+            System.out.println("결과 " + result);
 
-            throw new FileNotFoundException();
         } catch (InputMismatchException e)
         {
-            System.out.println("입력 관련 예외");
-            e.printStackTrace();
-        } catch (FileNotFoundException e)
+            System.out.println("숫자를 입력해주세요.");
+        } catch (ArithmeticException e)
         {
-            System.out.println("파일 찾을 수 없음 예외");
+            //각 exception 안에서도 각각의 상황의 따른 필터링이 필요함
+            if (e.getMessage().equals("by zero"))
+                System.out.println("숫자를 0으로 나눴습니다.");
             e.printStackTrace();
         } catch (Exception e)
         {
